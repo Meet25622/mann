a2ef90c1d71d7e8349942e85c269d355c4f67a13
Meet <vikanimeet9@gmail.com>
2022-08-23 20:41:17 +0530

    p2

---
+++ src/main/kotlin/p2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var a: Int = 10
+    var b: Double = a.toDouble()
+    println("$b")
+    var c: String = "545"
+    var d: Int = c.toInt()
+    println("$d")
+    var e: String = "44"
+    var f: Double = e.toDouble()
+    println("$f")
+}
